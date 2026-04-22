8edb68683104a4943cdef0b2fee4578c9afb30b7
gotr00t0day <48636787+gotr00t0day@users.noreply.github.com>
2026-04-21 20:51:52 -0400

    Update reliquary.cpp

--- reliquary.cpp
+++ reliquary.cpp
@@ -106,6 +106,17 @@ std::vector<std::string> runHistParser() {
 }
 
 int main() {
+    
+        std::cout << R"(
+  ____      _ _                              
+ |  _ \ ___| (_) __ _ _   _  __ _ _ __ _   _ 
+ | |_) / _ \ | |/ _` | | | |/ _` | '__| | | |
+ |  _ <  __/ | | (_| | |_| | (_| | |  | |_| |
+ |_| \_\___|_|_|\__, |\__,_|\__,_|_|   \__, |
+                |___/                  |___/ 
+          [ history credential harvester ]
+)" << "\n";
+    
     std::vector<std::string> historyResults = runHistParser();
     for (const auto& hResults : historyResults) {
         std::cout << hResults << "\n";
